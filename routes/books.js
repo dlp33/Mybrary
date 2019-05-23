a1d88fd143db44c070252fef09be2de45375ab16
Darren Paddock <darrenleepaddock@gmail.com>
2019-05-22 18:21:45 -0600

    Finished Author Routes

--- routes/books.js
+++ routes/books.js
@@ -1,90 +1,78 @@
-const express = require('express');
-const router = express.Router();
+    const express = require('express')
+    const router = express.Router()
+    const Book = require('../models/book')
+    const Author = require('../models/author')
+    const imageMimeTypes = ['image/jpeg', 'image/png', 'images/gif']
 
-const Book = require('../models/book');
-const Author = require('../models/author');
+    // All Books Route
+    router.get('/', async (req, res) => {
+      let query = Book.find()
+      if (req.query.title != null && req.query.title != '') {
+        query = query.regex('title', new RegExp(req.query.title, 'i'))
+      }
+      if (req.query.publishedBefore != null && req.query.publishedBefore != '') {
+        query = query.lte('publishDate', req.query.publishedBefore)
+      }
+      if (req.query.publishedAfter != null && req.query.publishedAfter != '') {
+        query = query.gte('publishDate', req.query.publishedAfter)
+      }
+      try {
+        const books = await query.exec()
+        res.render('books/index', {
+          books: books,
+          searchOptions: req.query
+        })
+      } catch {
+        res.redirect('/')
+      }
+    })
 
+    // New Book Route
+    router.get('/new', async (req, res) => {
+      renderNewPage(res, new Book())
+    })
 
-// All Books Route
-router.get('/', async (req, res) => {
-  let query = Book.find();
-  if (req.query.title != null && req.query.title != '') {
-    query = query.regex('title', newRegExp(req.query.title, 'i'))
-  }
-  if (req.query.publishedBefore != null && req.query.publishedBefore != '') {
-    query = query.lte('publishDate', req.query.publishedBefore);
-  }
-  if (req.query.publishedAfter != null && req.query.publishedAfter != '') {
-    query = query.gte('publishDate', req.query.publishedAfter);
-  }
-  try {
-    const books = await query.exec();
-    res.render('books/index', {
-      books: books,
-      searchOptions: req.query
-    });
+    // Create Book Route
+    router.post('/', async (req, res) => {
+      const book = new Book({
+        title: req.body.title,
+        author: req.body.author,
+        publishDate: new Date(req.body.publishDate),
+        pageCount: req.body.pageCount,
+        description: req.body.description
+      })
+      saveCover(book, req.body.cover)
 
-  } catch {
-    res.redirect('/');
+      try {
+        const newBook = await book.save()
+        // res.redirect(`books/${newBook.id}`)
+        res.redirect(`books`)
+      } catch {
+        renderNewPage(res, book, true)
+      }
+    })
 
-  }
-
-});
-
-// New Book Route
-router.get('/new', async (req, res) => {
-  renderNewPage(res, new Book());
-});
-
-// Create Book Route
-router.post('/', async (req, res) => {
-  const book = new Book({
-    title: req.body.title,
-    author: req.body.author,
-    publishDate: new Date(req.body.publishDate),
-    pageCount: req.body.pageCount,
-    description: req.body.description
-  })
-  saveCover(book, req.body.cover);
-
-
-
-  try {
-    console.log('inside the try');
-    const newBook = await book.save();
-    console.log('newBook = ' + newBook);
-    // res.redirect(`books/${newBook.id}`)
-    res.redirect('books');
-  } catch {
-    renderNewPage(res, book, true);
-  }
-});
-
-
-
-async function renderNewPage(res, book, hasError = false) {
-  try {
-    const authors = await Author.find({});
-    const params = {
-      authors: authors,
-      book: book
+    async function renderNewPage(res, book, hasError = false) {
+      try {
+        const authors = await Author.find({})
+        const params = {
+          authors: authors,
+          book: book
+        }
+        if (hasError) params.errorMessage = 'Error Creating Book'
+        res.render('books/new', params)
+      } catch {
+        res.redirect('/books')
+      }
     }
-    if (hasError) {
-      params.errorMessage = 'Error Creating Book';
+
+    function saveCover(book, coverEncoded) {
+      if (coverEncoded == null) return;
+      const cover = JSON.parse(coverEncoded)
+      if (cover != null && imageMimeTypes.includes(cover.type)) {
+        book.coverImage = new Buffer.from(cover.data, 'base64')
+        book.coverImageType = cover.type
+      }
     }
-    res.render('books/new', params);
-  } catch {
-    res.redirect('/books');
-  }
-};
 
-function saveCover(book, coverEncoded) {
-  if (coverEncoded == null) return;
-  const cover = JSON.parse(coverEncoded);
-  if (cover != null && imageMimeTypes.includes(cover.type)) {
-    book.coverImage = new Buffer.from(cover.data, 'base64');
-    book.coverImageType = cover.type;
-  }
-}
-
-module.exports = router;
+    module.exports = router
--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 
 // relative path ./ === relative to where we are. 
 const indexRouter = require('./routes/index');
@@ -23,6 +24,7 @@ app.use(bodyParser.urlencoded({
   limit: '10mb',
   extended: false
 }));
+app.use(methodOverride('_method'));
 
 const mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE_URL, {
